605a577df07969dda93bdff5d4f3d0b69e0093be
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2020-10-04 14:12:43 -0500

    tut sys

---
+++ Assets/Scripts/Player/GodTrigger.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable 649
+using UnityEngine;
+
+public class GodTrigger : MonoBehaviour
+{
+	[SerializeField] PlayerGod triggers;
+	[SerializeField] Transform position;
+	[SerializeField] float flyTime;
+	[SerializeField, TextArea] string text;
+
+	bool hasBeenTriggered;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!hasBeenTriggered && other.CompareTag("Player"))
+		{
+			hasBeenTriggered = true;
+			triggers.GoToPosAndSay(position.position, flyTime, text, 1);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerGod.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable 649
+using UnityEngine;
+using DG.Tweening;
+using Ciber_Turtle.UI;
+
+public class PlayerGod : MonoBehaviour
+{
+	[SerializeField] UIBitText text;
+
+	DG.Tweening.Core.TweenerCore<Vector3, Vector3, DG.Tweening.Plugins.Options.VectorOptions> tweener;
+
+	public void GoToPosAndSay(Vector3 position, float flyTime, string message, float messageTime)
+	{
+		if (tweener != null) tweener.Kill(false);
+		text.text = " ";
+		transform.localScale = new Vector3(position.x - transform.position.x < 0 ? -1 : 1, 1, 1);
+		tweener = transform.DOMove(position, flyTime, false).OnComplete(() => Say(message, messageTime));
+	}
+
+	public void Say(string message, float time)
+	{
+		text.text = message;
+	}
+}
