fa3efc7f2eaf6cb2bdfbdc6243eda427406848c7
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2020-10-09 11:59:00 -0500

    fixes to preformance

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -5,6 +5,19 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	[SerializeField] int defaultQuality = 1;
+	[SerializeField] Toggle togQuality;
+
+	void Awake()
+	{
+		if (PlayerPrefs.GetInt("quality.level", defaultQuality) != QualitySettings.GetQualityLevel())
+		{
+			QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality.level", defaultQuality));
+		}
+
+		togQuality.isOn = PlayerPrefs.GetInt("quality.level", defaultQuality) == 0 ? false : true;
+	}
+
 	public void Play()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -23,5 +36,11 @@ public class MainMenu : MonoBehaviour
 	public void Pause(bool state)
 	{
 		FindObjectOfType<Game>().IS_PAUSED = state;
+	}
+
+	public void SetQuility(bool high)
+	{
+		QualitySettings.SetQualityLevel(high ? 1 : 0, true);
+		PlayerPrefs.SetInt("quality.level", high ? 1 : 0);
 	}
 }
